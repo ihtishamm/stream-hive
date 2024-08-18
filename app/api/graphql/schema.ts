d381fcef5c5893914d6d17467f231316d7d5c3af
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-08-18 14:15:15 -0700

    remove the dummy schema

--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,32 +1,7 @@
 const schema = `#graphql
 
  type Query {
-    users: [User!]!
-    user(id: ID!): User
-    posts: [Post!]!
-    post(id: ID!): Post
-  }
-
-  type Mutation {
-    createUser(email: String!, name: String, password: String!): User!
-    createPost(title: String!, content: String!, authorId: String!): Post!
-  }
-
-  type User {
-    id: ID!
-    email: String!
-    name: String
-    posts: [Post!]!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    content: String!
-    published: Boolean!
-    image: String
-    author: User!
-    createdAt: String!
+    test:String!
   }
 `
 export default schema;
