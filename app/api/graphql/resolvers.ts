c764fa6d19f2ccbd69d6fc2e38cb963462d92612
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-08-20 02:23:40 -0700

    write the resolvers for creating the user, login user and get the current user

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,6 +1,37 @@
+import { GQLContext } from "@/types";
+import { signin, getUserFromToken, signup } from "@/lib/auth";
+import { GraphQLError } from "graphql";
+
 const resolvers = {
   Query: {
-    test: () => 'Hello World!',
+    me: async (_:any, __:any, ctx:GQLContext) => {
+      return ctx.user
+    }
+  },
+  Mutation: {
+    createUser: async (_:any, args) => {
+      const data = await signup(args.input)
+
+      if (!data || !data.user || !data.token) {
+        throw new GraphQLError('could not create user', {
+          extensions: { code: 'AUTH_ERROR' },
+        })
+      }
+
+      return { ...data.user, token: data.token }
+    },
+    signin: async (_:any, args) => {
+      const data = await signin(args.input)
+
+      if (!data || !data.user || !data.token) {
+        throw new GraphQLError('UNAUTHORIZED', {
+          extensions: { code: 'AUTH_ERROR' },
+        })
+      }
+
+      return { ...data.user, token: data.token }
+    },
+     
   },
 };
 
