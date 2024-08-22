d498956c721c8d02e645faf85cbd8aabc88e48bf
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-08-22 11:42:04 -0700

    write the schema for announcement type

--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,15 +1,22 @@
 const schema = `#graphql
 
    type User {
-   
     id: ID!
     name: String!
     email: String!
     createdAt: String!
     token: String
+    Announcements: [Announcement]!
     }
+
+     type Announcement {
+        id: ID!
+        message:String!
+        createdAt: String!
+        user: User!
+     }
       
-     input singInInput {
+     input signInInput {
     email: String!
     password: String!
   }
@@ -20,16 +27,29 @@ const schema = `#graphql
     password: String!
   }
 
+   input createAnnouncementInput {
+      message: String!
+  }
+      
+  input editAnnouncementInput {
+      id: ID!
+    message: String!
+  }
 
  type Query {
     me: User
+    getAllAnnouncements: [Announcement!]!
+    getUserAnnouncements(userid: ID!): [Announcement!]!
   }
 
   type Mutation {
-    signIn(input: singInInput!): User
+    signIn(input: signInInput!): User
     createUser(input: signUpInput!): User
+    createAnnouncement(input: createAnnouncementInput!): Announcement!
+    editAnnouncement(input: editAnnouncementInput!): Announcement!
+    deleteAnnouncement(id: ID!): ID!
   }
 
-  
 `
+
 export default schema;
