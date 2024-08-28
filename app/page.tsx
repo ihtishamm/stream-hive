0b03850721035162e0908edd50a4e7b5c5f62dd3
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-08-28 09:23:26 -0700

    add the lucide-react library

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default  function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center w-full flex-1 px-20 text-center">
     
-       <h1>hiii thereee</h1>
+       <h1>hiii thereee now let's build UI</h1>
   </main>
   );
 }
