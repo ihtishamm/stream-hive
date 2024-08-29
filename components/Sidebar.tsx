4b38bf405f7def77581279a50183d2ba49a2449c
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-08-29 04:49:27 -0700

    design the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,11 +23,11 @@ import {
     Podcast,
   } from "lucide-react"
   import { Children, ElementType, ReactNode, useState } from "react"
-  // import { Button, buttonStyles } from "../components/Button"
+ import { Button, buttonStyles } from "./Button";
   import { twMerge } from "tailwind-merge"
 import { playlists, subscriptions } from "@/dummy-data/Sidebar"
  import {useSidebarContext} from "../contexts/sidebarContext"
-  // import { PageHeaderFirstSection } from "./Header"
+  import { PageHeaderFirstSection } from "./Header"
   
   export function Sidebar() {
     const { isLargeOpen, isSmallOpen, close } = useSidebarContext()
@@ -60,7 +60,7 @@ import { playlists, subscriptions } from "@/dummy-data/Sidebar"
           } ${isSmallOpen ? "flex z-[999] bg-white max-h-screen" : "hidden"}`}
         >
           <div className="lg:hidden pt-2 pb-4 px-2 sticky top-0 bg-white">
-            {/* <PageHeaderFirstSection /> */}
+            <PageHeaderFirstSection />
           </div>
           <LargeSidebarSection>
             <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
@@ -173,10 +173,10 @@ import { playlists, subscriptions } from "@/dummy-data/Sidebar"
     return (
       <a
         href={url}
-        // className={twMerge(
-        //   buttonStyles({ variant: "ghost" }),
-        //   "py-4 px-1 flex flex-col items-center rounded-lg gap-1"
-        // )}
+        className={twMerge(
+          buttonStyles({ variant: "ghost" }),
+          "py-4 px-1 flex flex-col items-center rounded-lg gap-1"
+        )}
       >
         <Icon className="w-6 h-6" />
         <div className="text-sm">{title}</div>
@@ -207,7 +207,7 @@ import { playlists, subscriptions } from "@/dummy-data/Sidebar"
       <div>
         {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
         {visibleChildren}
-        {/* {showExpandButton && (
+        {showExpandButton && (
           <Button
             onClick={() => setIsExpanded(e => !e)}
             variant="ghost"
@@ -216,7 +216,7 @@ import { playlists, subscriptions } from "@/dummy-data/Sidebar"
             <ButtonIcon className="w-6 h-6" />
             <div>{isExpanded ? "Show Less" : "Show More"}</div>
           </Button>
-        )} */}
+        )}
       </div>
     )
   }
@@ -237,12 +237,12 @@ import { playlists, subscriptions } from "@/dummy-data/Sidebar"
     return (
       <a
         href={url}
-        // className={twMerge(
-        //   buttonStyles({ variant: "ghost" }),
-        //   `w-full flex items-center rounded-lg gap-4 p-3 ${
-        //     isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : undefined
-        //   }`
-        // )}
+        className={twMerge(
+          buttonStyles({ variant: "ghost" }),
+          `w-full flex items-center rounded-lg gap-4 p-3 ${
+            isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : undefined
+          }`
+        )}
       >
         {typeof IconOrImgUrl === "string" ? (
           <img src={IconOrImgUrl} className="w-6 h-6 rounded-full" />
