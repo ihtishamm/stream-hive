946ddd8f43f4b77bc9d801ac2397d526138d76c5
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-08-30 16:25:12 -0700

    design the single playlist page

--- app/playlists/[playlistId]/page.tsx
+++ app/playlists/[playlistId]/page.tsx
@@ -1,7 +1,30 @@
+import { videos } from "@/dummy-data/Home";
+import { SearchVideoItem } from "@/components/SearchVideoItems";
+import { PlaylistInfoCard } from "@/components/PlaylistInfoCard";
+const playlist = {
+  id: "1",
+  title: "My First Playlist",
+  description: "This is my first playlist",
+  playlistThumbnail: `${videos[3].thumbnailUrl}`,
+  videoCount: 10,
+  createdAt: new Date()
+}
+
+   
+
 export default function SinglePlaylist(){
     return(
-        <div>
-            <h1>Single Playlsit</h1>
+        <div className="container mx-auto p-6">
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="lg:w-1/3 lg:sticky-card  lg:min-h-screen">
+            <PlaylistInfoCard playlist={playlist} />
+          </div>
+          <div className="lg:w-2/3">
+                {videos.map(video => (
+                    <SearchVideoItem key={video.id}  {...video}/>
+                ))}
+            </div>
+        </div>
         </div>
     )
 }
