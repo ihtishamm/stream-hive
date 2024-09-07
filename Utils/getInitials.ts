0b9ce6eed96af400e9f6c64d9434c853137ac54a
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-09-07 11:22:58 -0700

    write the helping functions to get initails and to get unique channnel name

---
+++ Utils/getInitials.ts
@@ -0,0 +1,7 @@
+function getInitials(name: string): string {
+    const nameParts = name.split(" ");
+    const initials = nameParts.map(part => part[0]).join("").toUpperCase();
+    return initials.length > 2 ? initials.slice(0, 2) : initials;
+}
+
+export default getInitials;
