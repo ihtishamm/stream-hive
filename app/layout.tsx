dbabf069053d054b4f0c691952902f6970d5e101
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-09-10 11:14:45 -0700

    add toaster context in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/contexts/sidebarContext";
 import GQLProvider from "./gqlProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <SidebarProvider>
           <GQLProvider>
             {children}
+            <Toaster />
           </GQLProvider>
         </SidebarProvider>
       </body>
