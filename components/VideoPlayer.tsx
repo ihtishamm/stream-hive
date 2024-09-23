ae0dafad91fd66f366c879b1f8c9858262e59118
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-09-23 12:42:49 -0700

    design the watch video page. with proper width and height to video player

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,27 +1,24 @@
-"use client"
+"use client";
 
 import { CldVideoPlayer } from 'next-cloudinary';
 import 'next-cloudinary/dist/cld-video-player.css';
 import { Video } from '@/types';
 
-
 export function VideoPlayer({ video, publicId }: { video: Video, publicId: string }) {
-
-
   return (
-    <CldVideoPlayer
-      id={video?.id}
-      width='50'
-      height='50'
-      src={publicId}
-      aiHighlightsGraph={true}
-      autoPlay={true}
-      pictureInPictureToggle={true}
-      autoShowRecommendations={true}
-      logo={false}
-    />
-
+    <div className="w-full max-w-[900px] mx-auto">
+      <div className="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden">
+        <CldVideoPlayer
+          id={video?.id}
+          src={publicId}
+          className='w-full h-full'
+          aiHighlightsGraph={true}
+          autoPlay={true}
+          pictureInPictureToggle={true}
+          autoShowRecommendations={true}
+          logo={false}
+        />
+      </div>
+    </div>
   );
-};
-
-
+}
