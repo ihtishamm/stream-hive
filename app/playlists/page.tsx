067aec63b7571e21e86aa76ef753c8377092658f
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-09-27 14:38:36 -0700

    set the playlist page

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -1,43 +1,31 @@
-import { PlaylistCardStack } from "@/components/PlaylistCardStack";
-import { videos } from "@/dummy-data/Home";
-
-const dummyPlaylists = [
-  {
-    id: 1,
-    thumbnailUrl: `${videos[3].thumbnailUrl}`,
-    videoCount: 10,
-    playlistName: "My First Playlist",
-    channelName: "Channel One",
-  },
-  {
-    id: 2,
-    thumbnailUrl: `${videos[4].thumbnailUrl}`,
-    videoCount: 20,
-    playlistName: "My Second Playlist",
-    channelName: "Channel Two",
-  },
-  {
-    id: 2,
-    thumbnailUrl: `${videos[4].thumbnailUrl}`,
-    videoCount: 20,
-    playlistName: "My Second Playlist",
-    channelName: "Channel Two",
-  },
-  {
-    id: 2,
-    thumbnailUrl: `${videos[4].thumbnailUrl}`,
-    videoCount: 20,
-    playlistName: "My Second Playlist",
-    channelName: "Channel Two",
-  },
-];
+"use client";
+import { UserPlaylistsStack } from "@/components/UserPlaylistsCard";
+import { isAuth } from "@/lib/token";
+import Link from "next/link";
 
 export default function Allplaylists() {
+  const isAuthenticated = isAuth();
 
   return (
     <div className="p-8">
-      <h1 className="text-2xl font-bold mb-6">All Playlists</h1>
-      {/* <PlaylistCardStack items={dummyPlaylists} /> */}
+      {!isAuthenticated ? (
+        <div className="flex flex-col items-center justify-center h-64">
+          <p className="text-xl mb-4">You need to Sign in to view your playlists.</p>
+          <Link href="/signin">
+            <button className="bg-primary-500 text-white px-4 py-2 rounded-lg">
+              Log In
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div className="p-8">
+
+            <h1 className="text-2xl font-bold mb-6">All Playlists</h1>
+            <UserPlaylistsStack />
+          </div>
+        </>
+      )}
     </div>
   );
 }
